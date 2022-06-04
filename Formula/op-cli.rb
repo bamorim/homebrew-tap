33d3cbd908abe50a72e9536e35f1e84468b93bd0
Bernardo Amorim <bamorim2@gmail.com>
2022-06-04 15:00:59 +0100

    fix: file is now .zip

--- Formula/op-cli.rb
+++ Formula/op-cli.rb
@@ -5,7 +5,7 @@ class OpCli < Formula
 
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_linux_amd64_v#{version}.tar.gz"
+      url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_linux_amd64_v#{version}.zip"
       sha256 "cf68cc312f96aef1e9b69496b4c42fa581473f00b8ce0965b876702e8dd965d6"
     end
   end
